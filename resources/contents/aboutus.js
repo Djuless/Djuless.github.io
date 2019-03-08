9c4262cf4c110d2f1573065148cb4eb76124604f
Djules <julien.nacci@gmail.com>
2019-03-08 13:09:40 -0300

    feat: new about us content

--- resources/contents/aboutus.js
+++ resources/contents/aboutus.js
@@ -4,11 +4,15 @@ var aboutus = [
         content: "Cerveza Oceánica nace en el corazón de una familia de Playa Hermosa con el objetivo de elaborar cervezas inspiradas en la naturaleza uruguaya, utilizando materias primas agroecológicas y métodos de elaboración sustentables."
     },
     {
-        title: "Naturaleza y ciencia",
-        content: "Todas las materias prima utilizadas así como las prácticas de elaboración aplicadas se ajustan a las normas de agroecología internacionales, certificadas por la Red de Agroecología en Uruguay. Además se incorpora la investigación científica para el desarrollo de nuevas levaduras en Uruguay. Conoce más del proyecto biotecnológico <a href='http://www.levtec.com.uy'>aquí</a>."
+        title: "Naturaleza",
+        content: "Todas las materias prima utilizadas así como las prácticas de elaboración aplicadas se ajustan a las normas de agroecología internacionales, certificadas por la Red de Agroecología en Uruguay."
     },
     {
-        title: "El equipo",
-        content: "Cervecería Oceánica consta de un equipo de 7 personas. Los fundadores Agustina y Nicolás, comenzaron esta aventura hace 5 años. Agustina Olivera es investigadora especializada en la biología molecular de la levadura cervecera y Nicolás Collazo es maestro artesano. Juntos diseñan y elaboran las cervezas, investigan para generar nuevos sabores y aromas y mantienen el banco de levaduras cerveceras propias."
+        title: "Ciencia",
+        content: "Oceánica incorpora la investigación científica para el desarrollo de nuevas levaduras en Uruguay. Conoce más del proyecto biotecnológico <a href='http://www.levtec.com.uy'>aquí</a>."
+    },
+    {
+        title: "Los fundadores",
+        content: "Agustina y Nicolás comenzaron esta aventura hace 5 años. Agustina Olivera es investigadora especializada en la biología molecular de la levadura cervecera y Nicolás Collazo es maestro artesano. Juntos diseñan y elaboran las cervezas, investigan para generar nuevos sabores y aromas y mantienen el banco de levaduras cerveceras propias."
     }
 ];
